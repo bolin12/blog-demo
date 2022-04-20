de16e38c449963936bea92a3f001d99296cb6e8c
bolin12 <1569983927@qq.com>
2022-04-20 11:09:11 +0800

    dd

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  themeConfig: {
+    logo: 'assets/img/hero.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/about/' },
+      
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Group1', items: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/about/' },] },
+          { text: 'Group2', items: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/about/' },] }
+        ]
+      },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
